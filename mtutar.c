d8e55173aee3f7fa0df2382df7617c0259d4ad6e
Zachary Hill <zachary13hill@yahoo.com>
2016-01-31 12:10:37 -0500

    Added check for valid .mtu file

--- mtutar.c
+++ mtutar.c
@@ -12,32 +12,60 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
+#define FILENAME "example.mtu"
 
 int checkMagicBytes (FILE *fp) {
 
     int readCheck = 0;
+    char magicCheck[6] = "CS3411";
     char magicBytes[6];
-    char magicCheck[6];
-
-    magicCheck = "CS3411"
 
     readCheck = fread(&magicBytes, sizeof(char), 6, fp);
+    printf("%s\n", magicBytes);
 
     // check to make sure the file was read properly
     if (readCheck < 1) {
         printf("An error occured while Reading - Exiting\n");
         fclose(fp);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // check to make sure the file is a valid using the magicBytes
-
+    return strcmp(magicBytes, magicCheck);
 
 }
 
 int main (void) {
 
+    int byteCheck;
+
+    FILE *fp = fopen(FILENAME, "r");
+
+    // check for valid file open
+    if (fp == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    byteCheck = checkMagicBytes (fp);
+
+    if (byteCheck != 0) {
+        printf("Not a valid .mtu file - Exiting\n");
+        exit(1);
+    }
+
+    printf("Vaild .mtu file\n");
+
+    return 0;
 
 }
+
+/*
+ * References
+ *  http://stackoverflow.com/questions/579734/assigning-strings-to-arrays-of-characters
+ *  http://www.programmingsimplified.com/c/program/print-string
+ */
